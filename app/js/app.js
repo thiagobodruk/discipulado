2e8def642bd91d87cac2463ec22c76888ed17a55
Thiago Bodruk <thiagobodruk@gmail.com>
2017-01-06 15:06:43 -0200

    Criado sistema de navegacao

--- app/js/app.js
+++ app/js/app.js
@@ -2,9 +2,23 @@ var App = function(){
 	this.menuState = false;
 }
 
+var app = new App();
 $.extend(App.prototype, {
 	init : function(){
 		this.setMenu();
+		this.setBooks();
+		this.loadPage("#home");
+		$(".menu").click(function(){
+			app.toggleMenu();
+		});
+		$("#black").click(function(){
+			app.toggleMenu();
+		});
+		$("#menu a").not("header a").click(function(){
+			var id = $(this).attr("href");
+			app.loadPage(id);
+			app.toggleMenu();
+		});
 	},
 	toggleMenu : function(){
 		if(this.menuState){
@@ -22,24 +36,38 @@ $.extend(App.prototype, {
 		}
 	},
 	setMenu : function(){
-		console.log("Setting menu...");
 		$("#menu ul li").click(function(){
 			$("#menu ul li").removeClass("active");
 			$(this).addClass("active");
 		});
 		$("<div/>").attr("id", "black").appendTo("#outer");
-	}
+	},
+	setBooks : function(){
+		var d = null;
+		$.ajax({
+			url: "books.xml",
+			dataType: "text",
+			success: function(data){
+				$(data).find("book").appendTo("#main");
+				$("book").each(function(){
+					$(this).prepend($("<h2/>").text($(this).attr("name")));
+				});
+				$("chapter").each(function(){
+					$(this).prepend($("<h3/>").text($(this).attr("name")));
+				});
+				$("book").hide();
+			}
+		});
+	},
+	loadPage : function(id){
+		$("#main").children().hide();
+		$(id).show();
+		$(window).scrollTop(0);
+	},
 });
 
-var app = new App();
 $(function(){
 	app.init();	
-	$(".menu").click(function(){
-		app.toggleMenu();
-	});
-	$("#black").click(function(){
-		app.toggleMenu();
-	});
 });
 
 
