cbc1bbac1840c614805e84fccbf4008bb05f3c4d
Thiago Bodruk <thiagobodruk@gmail.com>
2017-01-06 13:20:39 -0200

    Estruturas refeitas

--- app/js/app.js
+++ app/js/app.js
@@ -1,28 +1,46 @@
 var App = function(){
-	return false;
+	this.menuState = false;
 }
 
 $.extend(App.prototype, {
 	init : function(){
-		this.menu();
-		$.mobile.defaultPageTransition = "none";
+		this.setMenu();
 	},
-	menu : function(){
-		$("#menu").panel();
-		$(".menu").click(function(){
-			$("#menu").panel("toggle");
-		});
+	toggleMenu : function(){
+		if(this.menuState){
+			$("#menu").animate({
+				left : "-100%"
+			});
+			$("#black").hide();
+			this.menuState = false;
+		}else{
+			$("#menu").animate({
+				left : 0
+			});
+			$("#black").fadeIn();
+			this.menuState = true;
+		}
+	},
+	setMenu : function(){
+		console.log("Setting menu...");
 		$("#menu ul li").click(function(){
 			$("#menu ul li").removeClass("active");
 			$(this).addClass("active");
 		});
+		$("<div/>").attr("id", "black").appendTo("#outer");
 	}
 });
 
+var app = new App();
 $(function(){
-	var app = new App();
 	app.init();	
-})
+	$(".menu").click(function(){
+		app.toggleMenu();
+	});
+	$("#black").click(function(){
+		app.toggleMenu();
+	});
+});
 
 
 
