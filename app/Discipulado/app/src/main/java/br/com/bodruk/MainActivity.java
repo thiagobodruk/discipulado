d194ae34d57b6c9d1e6338acfaeed82127ac3074
Thiago Bodruk <thiagobodruk@Thiago-Bodruk.local>
2017-05-20 00:14:02 -0300

    Ajustes de projeto

---
+++ app/Discipulado/app/src/main/java/br/com/bodruk/MainActivity.java
@@ -0,0 +1,45 @@
+package br.com.bodruk;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Load an ad into the AdMob banner view.
+        AdView adView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .setRequestAgent("android_studio:ad_template").build();
+        adView.loadAd(adRequest);
+
+        // Set webView
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        String url = getString(R.string.url);
+        webView.loadUrl(url);
+        webView.setHorizontalScrollBarEnabled(false);
+
+    }
+
+    // Back button
+    @Override
+    public void onBackPressed() {
+        WebView webView = (WebView) findViewById(R.id.webView);
+        if (webView.isFocused() && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+            finish();
+        }
+    }
+
+}
